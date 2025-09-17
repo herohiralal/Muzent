2f5c54e24fb271bdd8d03a7b3fffc8ab5552d302
Rohan Jadav <ro1.darkstar7@gmail.com>
2025-09-17 06:39:39 +0530

    updatedincludes

--- Source/__Prelude.h
+++ Source/__Prelude.h
@@ -5,12 +5,14 @@
 // Includes ========================================================================
 
 #include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Compiler.h"
-#include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Warnings.h"
 #include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Platforms.h"
+#include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Warnings.h"
 #ifdef MZNT_IMPLEMENTATION
 #endif
 #include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Primitives.h"
 #include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Macros.h"
+#include "Dependencies/Panshilar/Source/Dependencies/PNSLR_Intrinsics/Collections.h"
+#include "Dependencies/Panshilar/Source/Panshilar.h"
 
 //-skipreflect
 #endif // MZNT_ENTRY_H =============================================================
