7fed079fd1db773fb0b121c52ab1f5bbd62ef99e
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-02-26 07:12:56 +0530

    this time fr fixed the cross-platformness of the shader

--- Source/DirectX12Renderer.cpp
+++ Source/DirectX12Renderer.cpp
@@ -159,7 +159,6 @@ MZNT_DirectX12Renderer* MZNT_CreateRenderer_DirectX12(MZNT_RendererConfiguration
             psoDesc.SampleDesc.Count                = 1;
             psoDesc.SampleMask                      = UINT_MAX;
             psoDesc.RasterizerState                 = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-            psoDesc.RasterizerState.FrontCounterClockwise = true;
             psoDesc.BlendState                      = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
             psoDesc.DepthStencilState.DepthEnable   = false;
             psoDesc.DepthStencilState.StencilEnable = false;
